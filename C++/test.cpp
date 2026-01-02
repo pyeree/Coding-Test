aba5bca01f3037c6948e8f2e0c43f2051335dcac
hyunwoo <dlagusdn0218@gmail.com>
2026-01-02 22:12:01 +0900

    BOJ 10871 : 더 효율적인 풀이, 클린코드는 아니다

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <limits>
+// 더 짧은 풀이 ( 개발과 코딩테스트가 다른 이유 : 시간 내에 더 짧고 명확하게 푸는것이 중요)
+#include <bits/stdc++.h> 
 
 using namespace std;
 
@@ -9,16 +7,12 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n,m;
+    int n,m,t;
     cin >> n >> m;
-    int arr[n];
     for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    for(int i = 0; i < n; i++) {
-        if(arr[i] < m) {
-            cout << arr[i] << " ";
+        cin >> t; // 여기서는 cin 버퍼에서 한 번에 값을 가져온다
+        if(t < m) {
+            cout << t << " ";  // 여기서는 cout 버퍼에 한 번에 담아두고 출력을 한다
         }
     }
 
